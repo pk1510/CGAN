dc69d11f0b9654dc946c947b02f94e036d077da4
pk1510 <63361815+pk1510@users.noreply.github.com>
2020-10-12 17:57:57 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+lr = 0.01
+batch_size = 500
+image_size = 64
+channels_img = 1
+channels_noise = 256
+num_epochs = 30
+beta1 = 0.5
+gpus = 1
+# For how many channels Generator and Discriminator should use
+features_d = 16
+features_g = 16
